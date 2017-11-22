df5be8a72abe1085023e9665412a44f69d2ddbbd
Xolovana <33298852+Xolovana@users.noreply.github.com>
2017-11-22 14:08:41 +0700

    Initial commit

---
+++ app/src/main/java/com/splash/acer/latuts/MainActivity.java
@@ -0,0 +1,23 @@
+package com.splash.acer.latuts;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
+
